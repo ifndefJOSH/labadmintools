2c327dc554cb111461885121cc02ef54c2788db2
Josh Jeppson <joshua.jeppson@usu.edu>
2024-07-11 15:07:32 -0600

    license: gplv3

--- src/Actions.py
+++ src/Actions.py
@@ -1,5 +1,25 @@
+'''
+A graphical tool that makes IT peoples' lives easier
+
+Written by Josh Jeppson (ifndefJOSH)
+
+Copyright 2024 (c) Josh Jeppson, Utah State University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import os
-from types import FunctionType
 
 from PyQt5 import *
 from PyQt5.QtCore import *  # type: ignore
@@ -338,17 +358,6 @@ class ActionList:
 	def setProgressWidgets(self, progressBar : QProgressBar | None = None, statusBar : QStatusBar | None = None):
 		self.__progressBar = progressBar
 		self.__statusBar = statusBar
-
-class ActionRunner(QRunnable):
-	def __init__(self, callback : FunctionType, *args, **kwargs):
-		super(ActionRunner, self).__init__()
-		self.__callback = callback
-		self.args = args
-		self.kwargs = kwargs
-
-	# @pyqtSlot
-	def run(self):
-		self.__callback()
 
 if __name__ == "__main__":
 	print("Cannot run this file!")
--- src/ui/MainWindow.py
+++ src/ui/MainWindow.py
@@ -1,4 +1,24 @@
 # -*- coding: utf-8 -*-
+'''
+A graphical tool that makes IT peoples' lives easier
+
+Written by Josh Jeppson (ifndefJOSH)
+
+Copyright 2024 (c) Josh Jeppson, Utah State University
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 from threading import Thread
 from PyQt5 import *
