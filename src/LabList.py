07ae0d7a57902577628d34bc58024d6059a5339c
Josh Jeppson <joshua.jeppson@usu.edu>
2024-07-23 11:47:40 -0600

    my lsp autoimported uname because I have a variable called that 🫠

--- src/LabList.py
+++ src/LabList.py
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
-from os import uname
 from types import FunctionType
 
 from PyQt5 import *
